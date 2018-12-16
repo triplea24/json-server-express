0bcd42e0fd88582dfbe047ae686aeff9937c21ee
Amir Ahangari <triplea1373@gmail.com>
2018-12-16 11:56:15 +0330

    Adding netlify-lambda and serverless-http

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var express = require("express");
-var jsonServer = require("json-server");
+"use strict";
+const express = require("express");
+const serverless = require("serverless-http");
+const jsonServer = require("json-server");
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
 // ...
 
+app.use(bodyParser);
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
 // server.use('/api', jsonServer.defaults());
 app.use("/api", jsonServer.router("db.json"));
 
-app.listen(3000);
+module.exports = app;
+module.exports.handler = serverless(app);
